836c2f1d59b171ab82a1d2f0e4be03445ebbab5c
andresmedtr <123752360+andresmedtr@users.noreply.github.com>
2023-12-16 10:53:01 -0500

    added fetch requests to make it work

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,45 +1,50 @@
-import React, { useContext } from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import { Link } from "react-router-dom";
-import { Context } from "../store/appContext";
+import React, { useContext } from 'react';
+import rigoImage from '../../img/rigo-baby.jpg';
+import { Link } from 'react-router-dom';
+import { Context } from '../store/appContext';
 
-const Card = ({data}) => {
-const {store, actions} = useContext(Context);
-console.log(store.people,"data personajes")
-  return (
-	data.map((person,index)=>(
-		<div className="card ">
-		<img src={rigoImage} className="card-img-top" alt="..."/>
-			<div className="card-body">
-				<h5 className="card-title">{person.name}</h5>
-				<p className="card-text">
-					<p className="p-0 m-0">Gender:</p>
-					<p className="p-0 m-0">Hair-color:</p>
-					<p className="p-0 m-0">Eye-color:</p>
-				</p>
-				<Link to="/demo">
-					<button className="btn btn-outline-primary pe-4">Learn more!</button>
-				</Link>
-				<button className="btn btn-light ms-5"><i className="fa-regular fa-heart" style={{color: "#ff0f0f",}}/></button>
-			</div>
-	</div>
-	))
-		// <div className="card ">
-		// 	<img src={rigoImage} className="card-img-top" alt="..."/>
-		// 		<div className="card-body">
-		// 			<h5 className="card-title">Character Name</h5>
-		// 			<p className="card-text">
-		// 				<p className="p-0 m-0">Gender:</p>
-		// 				<p className="p-0 m-0">Hair-color:</p>
-		// 				<p className="p-0 m-0">Eye-color:</p>
-		// 			</p>
-		// 			<Link to="/demo">
-		// 				<button className="btn btn-outline-primary pe-4">Learn more!</button>
-		// 			</Link>
-		// 			<button className="btn btn-light ms-5"><i className="fa-regular fa-heart" style={{color: "#ff0f0f",}}/></button>
-		// 		</div>
-		// </div>
-  );
+const Card = ({ data }) => {
+  const { store, actions } = useContext(Context);
+  console.log(store.people);
+  return data?.map((item, index) => (
+    <div key={index} className="card ">
+      <img
+        src={`https://starwars-visualguide.com/assets/img/characters/${item.uid}.jpg`}
+        className="card-img-top"
+        alt="..."
+      />
+      <div className="card-body">
+        <h5 className="card-title">
+          Name:
+          {item.properties.name.charAt(0).toUpperCase() +
+            item.properties.name.slice(1)}
+        </h5>
+        <div className="card-text">
+          <p className="p-0 m-0">
+            Gender:{' '}
+            {item.properties.gender.charAt(0).toUpperCase() +
+              item.properties.gender.slice(1)}
+          </p>
+          <p className="p-0 m-0">
+            Hair-color:{' '}
+            {item.properties.hair_color.charAt(0).toUpperCase() +
+              item.properties.hair_color.slice(1)}
+          </p>
+          <p className="p-0 m-0">
+            Eye-color:{' '}
+            {item.properties.eye_color.charAt(0).toUpperCase() +
+              item.properties.eye_color.slice(1)}
+          </p>
+        </div>
+        <Link to="/demo">
+          <button className="btn btn-outline-primary pe-4">Learn more!</button>
+        </Link>
+        <button className="btn btn-light ms-5">
+          <i className="fa-regular fa-heart" style={{ color: '#ff0f0f' }} />
+        </button>
+      </div>
+    </div>
+  ));
 };
 
 export default Card;
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,93 +1,81 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			],
-			people:[],
-			vehicles:[],
-			planets:[],
-			favorites:[]
-			
+  return {
+    store: {
+      demo: [
+        {
+          title: 'FIRST',
+          background: 'white',
+          initial: 'white',
+        },
+        {
+          title: 'SECOND',
+          background: 'white',
+          initial: 'white',
+        },
+      ],
+      people: [],
+      vehicles: [],
+      planets: [],
+      favorites: [],
+    },
+    actions: {
+      loadData: () => {
+        const store = getStore();
+        fetch('https://www.swapi.tech/api/people/')
+          .then((res) => res.json())
+          .then((data) => {
+            data.results.map((item) => {
+              fetch(`https://www.swapi.tech/api/people/${item.uid}`)
+                .then((res) => res.json())
+                .then((data2) => {
+                  setStore({ people: [...store.people, data2.result] });
+                });
+            });
+          })
+          .catch((err) => console.error(err));
 
-		},
-		actions: {
-			getDataCharacters: async () =>{
-				await fetch("https://www.swapi.tech/api/people")
-				.then(res => res.json())
-				.then(data => {
-					console.log(data)
-					setStore({people:data.results})
-				})
-				.catch(err => console.error(err))
-			},
-			getDataPlanets: () =>{
-				fetch("https://www.swapi.tech/api/planets")
-				.then(res => res.json())
-				.then(data => {
-					console.log(data)
-					setStore({planets:data.results})
-				})
-				.catch(err => console.error(err))
-			},
-			getDataVehicles: () =>{
-				fetch("https://www.swapi.tech/api/vehicles")
-				.then(res => res.json())
-				.then(data => {
-					console.log(data)
-					setStore({vehicles:data.results})
-				})
-				.catch(err => console.error(err))
-			}
+        fetch('https://www.swapi.tech/api/planets/')
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            setStore({ planets: data.results });
+          })
+          .catch((err) => console.error(err));
 
+        fetch('https://www.swapi.tech/api/vehicles/')
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            setStore({ vehicles: data.results });
+          })
+          .catch((err) => console.error(err));
+      },
 
+      // Use getActions to call a function within a fuction
+      // exampleFunction: () => {
+      // 	getActions().changeColor(0, "green");
+      // },
+      // loadSomeData: () => {
+      // 	/**
+      // 		fetch().then().then(data => setStore({ "foo": data.bar }))
+      // 	*/
+      // },
+      // changeColor: (index, color) => {
+      // 	//get the store
+      // 	const store = getStore();
 
+      // 	//we have to loop the entire demo array to look for the respective index
+      // 	//and change its color
+      // 	const demo = store.demo.map((elm, i) => {
+      // 		if (i === index) elm.background = color;
+      // 		return elm;
+      // 	});
 
-
-
-
-
-
-
-
-
-
-
-
-			// Use getActions to call a function within a fuction
-			// exampleFunction: () => {
-			// 	getActions().changeColor(0, "green");
-			// },
-			// loadSomeData: () => {
-			// 	/**
-			// 		fetch().then().then(data => setStore({ "foo": data.bar }))
-			// 	*/
-			// },
-			// changeColor: (index, color) => {
-			// 	//get the store
-			// 	const store = getStore();
-
-			// 	//we have to loop the entire demo array to look for the respective index
-			// 	//and change its color
-			// 	const demo = store.demo.map((elm, i) => {
-			// 		if (i === index) elm.background = color;
-			// 		return elm;
-			// 	});
-
-			// 	//reset the global store
-				// setStore({ demo: demo });
-			}
-		}
-	};
+      // 	//reset the global store
+      // setStore({ demo: demo });
+    },
+  };
+};
 // };
 
 export default getState;
